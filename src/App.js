5528fd8a4476a5b93e82658803907f10ecd15c69
bleak <ffeeaarrofdeath@mail.ru>
2024-08-31 19:25:35 +0300

    init

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+const Div = styled.div`
+  text-align: center;
+`;
+
+function App() {
+  return (
+    <Div>
+      <i className="fa fa-calendar"></i>
+      <div>456</div>
+    </Div>
+  );
+}
+
+export default App;
