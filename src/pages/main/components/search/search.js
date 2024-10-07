942edd28eea43daca2b9b86c92110a0b00ca3678
bleak <ffeeaarrofdeath@mail.ru>
2024-10-07 03:57:07 +0300

    Реализовать поиск на главной

---
+++ src/pages/main/components/search/search.js
@@ -0,0 +1,33 @@
+import styled from "styled-components";
+import { Icon, Input } from "../../../../components";
+
+const SearchContainer = ({ className, searchPhrase, onChange }) => {
+  return (
+    <div className={className}>
+      <Input
+        onChange={onChange}
+        value={searchPhrase}
+        placeholder="Поиск по заголовкам"
+      />
+      <Icon id="fa-search" size="21px" />
+    </div>
+  );
+};
+
+export const Search = styled(SearchContainer)`
+  display: flex;
+  margin: 40px auto 0;
+  width: 340px;
+  height: 40px;
+  position: relative;
+
+  & > div {
+    position: absolute;
+    right: 7px;
+    top: 6px;
+  }
+
+  & > input {
+    padding: 10px 38px 10px 10px;
+  }
+`;
--- src/pages/main/main.js
+++ src/pages/main/main.js
@@ -1,42 +1,60 @@
-import { useEffect, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import styled from "styled-components";
-import { Pagination, PostCard } from "./components";
+import { Pagination, PostCard, Search } from "./components";
 import { useServerRequest } from "../../hooks";
 import { PAGINATION_LIMIT } from "../../constans";
-import { getLastPageFromLinks } from "./utils";
+import { debounce, getLastPageFromLinks } from "./utils";
 
 const MainContainer = ({ className }) => {
   const [posts, setPosts] = useState([]);
   const [page, setPage] = useState(1);
   const [lastPage, setLastPage] = useState(1);
+  const [shouldSearch, setShouldSearch] = useState(false);
+  const [searchPhrase, setSearchPhrase] = useState("");
   const requestServer = useServerRequest();
 
   useEffect(() => {
-    requestServer("fetchPosts", page, PAGINATION_LIMIT).then(
+    requestServer("fetchPosts", searchPhrase, page, PAGINATION_LIMIT).then(
       ({ res: { posts, links } }) => {
         setPosts(posts);
         setLastPage(getLastPageFromLinks(links));
       }
     );
-  }, [requestServer, page]);
+  }, [requestServer, page, shouldSearch]);
+
+  const startDelayedSearch = useMemo(() => debounce(setShouldSearch, 2000), []);
+
+  const onSearch = ({ target }) => {
+    setSearchPhrase(target.value);
+    startDelayedSearch(!shouldSearch);
+  };
 
   return (
     <div className={className}>
-      <div className="post-list">
-        {posts.map(({ id, title, imageUrl, publishedAt, commentsCount }) => {
-          return (
-            <PostCard
-              key={id}
-              id={id}
-              title={title}
-              publishedAt={publishedAt}
-              commentsCount={commentsCount}
-              imageUrl={imageUrl}
-            />
-          );
-        })}
+      <div className="posts-and-search">
+        <Search searchPhrase={searchPhrase} onChange={onSearch} />
+        {posts.length ? (
+          <div className="post-list">
+            {posts.map(
+              ({ id, title, imageUrl, publishedAt, commentsCount }) => {
+                return (
+                  <PostCard
+                    key={id}
+                    id={id}
+                    title={title}
+                    publishedAt={publishedAt}
+                    commentsCount={commentsCount}
+                    imageUrl={imageUrl}
+                  />
+                );
+              }
+            )}
+          </div>
+        ) : (
+          <div className="no-posts-found">Статьи не найдены</div>
+        )}
       </div>
-      {lastPage > 1 && (
+      {lastPage > 1 && posts.length > 0 && (
         <Pagination lastPage={lastPage} setPage={setPage} page={page} />
       )}
     </div>
@@ -44,9 +62,19 @@ const MainContainer = ({ className }) => {
 };
 
 export const Main = styled(MainContainer)`
+  display: flex;
+  flex-direction: column;
+  justfiy-content: space-between;
+
   & .post-list {
     display: flex;
     flex-wrap: wrap;
-    padding: 20px;
+    padding: 20px 20px 80px;
+  }
+
+  & .no-posts-found {
+    text-align: center;
+    font-size: 18px;
+    margin-top: 30px;
   }
 `;
