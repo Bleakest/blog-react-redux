1204391fe21379d8638fc16fba9eed9a00c83619
Дмитрий Колесников <dmitrijkolesnikov@MacBook-Air-Dmitrij.local>
2024-11-04 00:37:47 +0300

    blog

--- blog-backend/controllers/post.js
+++ blog-backend/controllers/post.js
@@ -1,14 +1,24 @@
 const Post = require("../models/Post");
 
 //add
-function addPost(post) {
-  return Post.create(post);
+async function addPost(post) {
+  const newPost = await Post.create(post);
+
+  await newPost.populate({
+    path: "comments",
+    populate: "author",
+  });
 }
 //edit
 
 async function editPost(id, post) {
   const newPost = await Post.findByIdAndUpdate(id, post, {
     returnDocument: "after",
+  });
+
+  await newPost.populate({
+    path: "comments",
+    populate: "author",
   });
 
   return newPost;
@@ -24,21 +34,28 @@ async function getPosts(search = "", limit = 10, page = 1) {
       .limit(limit)
       .skip((page - 1) * limit)
       .sort({ createdAt: -1 }),
-      Post.countDocuments({ title: { $regex: search, $options: "i" } })
+    Post.countDocuments({ title: { $regex: search, $options: "i" } }),
   ]);
-  
+
   return {
-    posts,lastPage: Math.ceil(count / limit)
-  }
+    posts,
+    lastPage: Math.ceil(count / limit),
+  };
 }
 
 //get item
 
 function getPost(id) {
-  return Post.findById(id);
+  return Post.findById(id).populate({
+    path: "comments",
+    populate: "author",
+  });
 }
-
 
 module.exports = {
-    addPost, editPost, deletePost, getPosts, getPost
-}
+  addPost,
+  editPost,
+  deletePost,
+  getPosts,
+  getPost,
+};
